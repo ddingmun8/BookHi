2b5261cc8a175c49e250ab2f779bf235d146c819
ddingmun8 <ddingmun8@gmail.com>
2022-02-20 13:31:26 +0900

    BookHi main 화면 : viewpager create

---
+++ app/src/main/java/com/ddingmung/bookhi/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.ddingmung.bookhi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val btnLogin : Button = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            btnLoginClick()
+        }
+        
+    }
+    fun btnLoginClick(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+}

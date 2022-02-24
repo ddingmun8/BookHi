08268ebb8ab9ef7a298b3f6368138b3d613b0d3e
ddingmun8 <ddingmun8@gmail.com>
2022-02-24 22:52:15 +0900

    화면 fragment 로 수정 -> viewPager2 적용

--- app/src/main/java/com/ddingmung/bookhi/MainActivity.kt
+++ app/src/main/java/com/ddingmung/bookhi/MainActivity.kt
@@ -2,99 +2,37 @@ package com.ddingmung.bookhi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
-import kotlinx.android.synthetic.main.book_list.view.*
-
-
-private val MIN_SCALE = 0.85f // 뷰가 몇퍼센트로 줄어들 것인지
-private val MIN_ALPHA = 0.5f // 어두워지는 정도
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* 여백, 너비에 대한 정의 */
-        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.pageMargin)
-        val pagerWidth = resources.getDimensionPixelOffset(R.dimen.pageWidth)
-        val screenWidth = resources.displayMetrics.widthPixels // 스마트폰의 너비 길이를 가져옴
-        val offsetPx = screenWidth - pageMarginPx - pagerWidth
+        //시작하면 mainFragment Show
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainFragment, ViewPagerFragment())
+            .commit()
 
-        // 뷰 페이저에 들어갈 아이템
-        fun getIdolList(): ArrayList<Int> {
-            return arrayListOf<Int>(R.drawable.bookhi_nick_logo_final, R.drawable.bookhi_logo_version2, R.drawable.bookhi_nick_logo_final)
+        val btnShowMain : Button = findViewById(R.id.btnShowMain)
+        val btnRegBook : Button = findViewById(R.id.btnRegBook)
+        val btnShowMyPage : Button = findViewById(R.id.btnShowMyPage)
+
+        btnShowMain.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.mainFragment, MainFragment())
+                .commit()
         }
-
-        val viewPager_book : ViewPager2 = findViewById(R.id.viewPager_book)
-        viewPager_book.adapter = ViewPagerAdapter(getIdolList()) // 어댑터 생성
-        viewPager_book.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
-        viewPager_book.setPageTransformer(ZoomOutPageTransformer()) // 애니메이션 적용
-        viewPager_book.offscreenPageLimit = 1 // 몇 개의 페이지를 미리 로드 해둘것인지
-
-        viewPager_book.setPageTransformer { page, position ->
-            page.translationX = position * -offsetPx
+        btnRegBook.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.mainFragment, RegBookFragment())
+                .commit()
         }
-
-    }
-}
-
-class ZoomOutPageTransformer : ViewPager2.PageTransformer {
-    override fun transformPage(view: View, position: Float) {
-        view.apply {
-            val pageWidth = width
-            val pageHeight = height
-            when {
-                position < -1 -> { // [-Infinity,-1)
-                    // This page is way off-screen to the left.
-                    alpha = 0f
-                }
-                position <= 1 -> { // [-1,1]
-                    // Modify the default slide transition to shrink the page as well
-                    val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
-                    val vertMargin = pageHeight * (1 - scaleFactor) / 2
-                    val horzMargin = pageWidth * (1 - scaleFactor) / 2
-                    translationX = if (position < 0) {
-                        horzMargin - vertMargin / 2
-                    } else {
-                        horzMargin + vertMargin / 2
-                    }
-
-                    // Scale the page down (between MIN_SCALE and 1)
-                    scaleX = scaleFactor
-                    scaleY = scaleFactor
-
-                    // Fade the page relative to its size.
-                    alpha = (MIN_ALPHA +
-                            (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
-                }
-                else -> { // (1,+Infinity]
-                    // This page is way off-screen to the right.
-                    alpha = 0f
-                }
-            }
+        btnShowMyPage.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.mainFragment, MyPageFragment())
+                .commit()
         }
-    }
-}
-
-class ViewPagerAdapter(bookList: ArrayList<Int>) : RecyclerView.Adapter<ViewPagerAdapter.PagerViewHolder>() {
-    var item = bookList
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
-
-    override fun getItemCount(): Int = item.size
-
-    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
-        holder.book.setImageResource(item[position])
-    }
-
-    inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
-        (LayoutInflater.from(parent.context).inflate(R.layout.book_list, parent, false)){
-
-        val book = itemView.imageView_book!!
 
     }
 }
---
+++ app/src/main/java/com/ddingmung/bookhi/ViewPagerFragment.kt
@@ -0,0 +1,135 @@
+package com.ddingmung.bookhi
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.fragment_view_pager.*
+import kotlin.math.abs
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ViewPagerFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ViewPagerFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_pager, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ViewPagerFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ViewPagerFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // Adapter를 생성하면서 넘길 색상이 담긴 ArrayList<Int> 생성
+        var bgColors = arrayListOf<Int>(
+            R.color.gray,
+            R.color.nick,
+            R.color.purple_200,
+            R.color.light_nick,
+            R.color.purple_700
+        )
+
+        // RecyclerView.Adapter<PagerViewHolder>()
+        viewPager.adapter = PagerRecyclerAdapter(bgColors)
+        // 관리하는 페이지 수. default = 1
+        viewPager.offscreenPageLimit = 4
+        // item_view 간의 양 옆 여백을 상쇄할 값
+        val offsetBetweenPages = resources.getDimensionPixelOffset(R.dimen.offsetBetweenPages).toFloat()
+        viewPager.setPageTransformer { page, position ->
+            val myOffset = position * -(2 * offsetBetweenPages)
+            if (position < -1) {
+                page.translationX = -myOffset
+            } else if (position <= 1) {
+                // Paging 시 Y축 Animation 배경색을 약간 연하게 처리
+                val scaleFactor = 0.8f.coerceAtLeast(1 - abs(position))
+                page.translationX = myOffset
+                page.scaleY = scaleFactor
+                page.alpha = scaleFactor
+            } else {
+                page.alpha = 0f
+                page.translationX = myOffset
+            }
+        }
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                Log.e("ViewPagerFragment", "Page ${position+1}")
+            }
+        })
+    }
+}
+
+class PagerRecyclerAdapter(private val bgColors: ArrayList<Int>) : RecyclerView.Adapter<PagerRecyclerAdapter.PagerViewHolder>() {
+
+    inner class PagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val pageName: TextView = itemView.findViewById(R.id.pageName)
+
+        fun bind(@ColorRes bgColor: Int, position: Int) {
+            pageName.text = "Page ${position+1}"
+            pageName.setBackgroundColor(ContextCompat.getColor(pageName.context, bgColor))
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_view,
+            parent,
+            false
+        )
+        return PagerViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+        holder.bind(bgColors[position], position)
+    }
+
+    override fun getItemCount(): Int = bgColors.size
+}

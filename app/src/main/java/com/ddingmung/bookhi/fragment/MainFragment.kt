62b6edd266be59274a8631bf311d761a2893c6a1
ddingmun8 <ddingmun8@gmail.com>
2022-03-01 16:23:15 +0900

    Navigation Jetpack 사용

--- app/src/main/java/com/ddingmung/bookhi/fragment/MainFragment.kt
+++ app/src/main/java/com/ddingmung/bookhi/fragment/MainFragment.kt
@@ -1,4 +1,4 @@
-package com.ddingmung.bookhi
+package com.ddingmung.bookhi.fragment
 
 import android.os.Bundle
 import android.util.Log
@@ -6,12 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
-import kotlinx.android.synthetic.main.fragment_view_pager.*
+import com.ddingmung.bookhi.R
+import kotlinx.android.synthetic.main.book_list.view.*
+import kotlinx.android.synthetic.main.fragment_main.*
 import kotlin.math.abs
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,12 +26,14 @@ import kotlin.math.abs
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+lateinit var navController: NavController
+
 /**
  * A simple [Fragment] subclass.
- * Use the [ViewPagerFragment.newInstance] factory method to
+ * Use the [MainFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ViewPagerFragment : Fragment() {
+class MainFragment : Fragment(){
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -42,7 +51,7 @@ class ViewPagerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_pager, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     companion object {
@@ -57,7 +66,7 @@ class ViewPagerFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ViewPagerFragment().apply {
+            MainFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
@@ -132,4 +141,6 @@ class PagerRecyclerAdapter(private val bgColors: ArrayList<Int>) : RecyclerView.
     }
 
     override fun getItemCount(): Int = bgColors.size
+
 }
+

4fff567d32b1516921d182bd5e4abeae1d769b27
ddingmun8 <ddingmun8@gmail.com>
2022-03-01 16:23:04 +0900

    Navigation Jetpack 사용

--- app/src/main/java/com/ddingmung/bookhi/MainActivity.kt
+++ app/src/main/java/com/ddingmung/bookhi/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.ddingmung.bookhi
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import androidx.navigation.NavController
+import com.ddingmung.bookhi.fragment.MainFragment
+import com.ddingmung.bookhi.fragment.MyPageFragment
+import com.ddingmung.bookhi.fragment.RegBookFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //시작하면 mainFragment Show
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.mainFragment, ViewPagerFragment())
-            .commit()
+        /*supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, ViewPagerFragment())
+            .commit()*/
 
         val btnShowMain : Button = findViewById(R.id.btnShowMain)
         val btnRegBook : Button = findViewById(R.id.btnRegBook)
@@ -20,19 +30,19 @@ class MainActivity : AppCompatActivity() {
 
         btnShowMain.setOnClickListener {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.mainFragment, MainFragment())
+                .replace(R.id.nav_host_fragment, MainFragment())
                 .commit()
         }
         btnRegBook.setOnClickListener {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.mainFragment, RegBookFragment())
+                .replace(R.id.nav_host_fragment, RegBookFragment())
                 .commit()
         }
         btnShowMyPage.setOnClickListener {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.mainFragment, MyPageFragment())
+                .replace(R.id.nav_host_fragment, MyPageFragment())
                 .commit()
         }
-
     }
 }
+
--- app/src/main/java/com/ddingmung/bookhi/fragment/MyPageFragment.kt
+++ app/src/main/java/com/ddingmung/bookhi/fragment/MyPageFragment.kt
@@ -1,18 +1,11 @@
-package com.ddingmung.bookhi
+package com.ddingmung.bookhi.fragment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.fragment.app.FragmentActivity
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
-import kotlinx.android.synthetic.main.book_list.view.*
+import com.ddingmung.bookhi.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,15 +14,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
+ * Use the [MyPageFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainFragment : Fragment() {
+class MyPageFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    private var viewPager_book: ViewPager2? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,11 +35,8 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_my_page, container, false)
     }
-
-
 
     companion object {
         /**
@@ -57,12 +45,12 @@ class MainFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
+         * @return A new instance of fragment MyPageFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
+            MyPageFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
@@ -70,27 +58,3 @@ class MainFragment : Fragment() {
             }
     }
 }
-
-
-class PagerFragmentStateAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity){
-    var fragments : ArrayList<Fragment> = ArrayList()
-
-    override fun getItemCount(): Int {
-        return fragments.size
-    }
-
-    override fun createFragment(position: Int): Fragment {
-        return fragments[position]
-    }
-
-    fun addFragment(fragment: Fragment){
-        fragments.add(fragment)
-        notifyItemInserted(fragments.size - 1)
-    }
-
-    fun removeFragment(){
-        fragments.removeLast()
-        notifyItemRemoved(fragments.size)
-    }
-}
-
